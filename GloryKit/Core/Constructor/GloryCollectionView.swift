f1130ab1e1eb3cc07ee190b30b3dabbcb1f3a1c3
Krisnandika <krisnandikaaji@gmail.com>
2019-12-16 18:37:44 +0700

    Add skeletonview; Add GloryTableView; Add GloryCollectionView

---
+++ GloryKit/Core/Constructor/GloryCollectionView.swift
@@ -0,0 +1,17 @@
+//
+//  GloryCollectionView.swift
+//  GloryKit
+//
+//  Created by Krisnandika Aji on 16/12/19.
+//  Copyright © 2019 Krisnandika Aji. All rights reserved.
+//
+
+import Foundation
+import SnapKit
+import SkeletonView
+
+// TODO: Finalize this.
+/// GloryCollectionView is
+public class GloryCollectionView: UITableView {
+    
+}
---
+++ GloryKit/Core/Constructor/GloryTableView.swift
@@ -0,0 +1,17 @@
+//
+//  GloryTableView.swift
+//  GloryKit
+//
+//  Created by Krisnandika Aji on 16/12/19.
+//  Copyright © 2019 Krisnandika Aji. All rights reserved.
+//
+
+import Foundation
+import SnapKit
+import SkeletonView
+
+// TODO: Finalize this.
+/// GloryTableView is
+public class GloryTableView: UITableView {
+    
+}
